cf67cb992499e763af62bde94d249f3a736b7a4e
mengfoobar <meng.foo.bar@gmail.com>
2019-07-27 11:38:33 -0700

    added show page added gravatar allow custom size for gravatar
    added initial form
    ensured params passed in for create only has the required fields
    
    handling invalid cases for signups
    - rendering error messages
    
    added signup success workflow
    added test for successful signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+      }}
+    end
+  end
+
+  test "valid signup information" do
+    get signup_path
+
+    # TODO: verify if these all run asynchronously like nodejs async
+
+    post users_path, params: { user: {
+        name: "Henry Lao",
+        email: "user@valid.com",
+        password: "foobar",
+        password_confirmation: "foobar"
+    }}
+
+    assert_equal User.count, 1
+    follow_redirect! #TODO; hella magical. look into
+    assert_template 'users/show'
+
+  end
+end
