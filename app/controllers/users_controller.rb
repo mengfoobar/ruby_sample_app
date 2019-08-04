2fd7f7f93fe7e85ecb3abd9da2bf7d83d6bcd657
mengfoobar <meng.foo.bar@gmail.com>
2019-08-03 19:54:48 -0700

    added update in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
+
   private
+
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
