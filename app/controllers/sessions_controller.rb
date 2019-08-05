d2667d0dc00c223fce85ceca3a144c5cb4cf18ca
mengfoobar <meng.foo.bar@gmail.com>
2019-08-04 17:12:29 -0700

    added friendly forwarding and related rest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log user in and redirect to user's show page
       log_in user
-      redirect_to user
+      redirect_back_or user
     else
       # create an error message
       flash.now[:danger] = 'Invalid email/password combination'
