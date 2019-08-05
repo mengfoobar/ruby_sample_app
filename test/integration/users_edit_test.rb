b472ff149e234e2fbf96a4133fca4c0e7812839a
mengfoobar <meng.foo.bar@gmail.com>
2019-08-04 17:04:10 -0700

    added redirect for users trying to access pages not their own

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,14 +3,15 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @main_user = users(:michael)
+    @secondary_user = users(:henry)
   end
 
   test "unsuccessful edit" do
-    log_in_as(@user)
-    get edit_user_path(@user)
+    log_in_as(@main_user)
+    get edit_user_path(@main_user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user:{ name:"",
+    patch user_path(@main_user), params: { user:{ name:"",
                                              email: "foo@invalid",
                                              password: "foo",
                                              password_confirmation: "bar"}}
@@ -18,32 +19,32 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
-    log_in_as(@user)
-    get edit_user_path(@user)
+    log_in_as(@main_user)
+    get edit_user_path(@main_user)
     assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: {name: name,
+    patch user_path(@main_user), params: { user: {name: name,
                                              email: email,
                                              password: "",
                                              password_confirmation: ""
     }}
     assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal name, @user.name
-    assert_equal email, @user.email
+    assert_redirected_to @main_user
+    @main_user.reload
+    assert_equal name, @main_user.name
+    assert_equal email, @main_user.email
   end
 
   test "user can't access edit while logged out" do
-    get edit_user_path(@user)
+    get edit_user_path(@main_user)
     assert_not flash[:danger].empty?
     assert_redirected_to login_url
   end
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: {user: { name: @user.name,
-                                             email: @user.email
+    patch user_path(@main_user), params: {user: { name: @main_user.name,
+                                             email: @main_user.email
     }}
     assert_not flash.empty?
     assert_redirected_to login_url
