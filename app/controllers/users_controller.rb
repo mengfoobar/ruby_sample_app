da960ffdc28a08ac2bd0f6f5d6928ff19c8347b8
mengfoobar <meng.foo.bar@gmail.com>
2019-08-07 21:15:33 -0700

    added admin migration field admins can now view and delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update] # allows us to trigger logged_in_user only for edit, update
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy] # allows us to trigger logged_in_user only for edit, update
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page]).per_page(10)
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User delete"
+    redirect_to users_url
+  end
+
   def logged_in_user
     unless logged_in?
       store_location
@@ -58,4 +65,8 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
