24e69df5444e771d252e25e92be6fe670ae11adf
mengfoobar <meng.foo.bar@gmail.com>
2019-08-18 16:14:50 -0700

    added create and destroy routes for micropost ensured that user is logged in move logged_in check to applicaiton control level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,14 +48,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
@@ -67,7 +59,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
